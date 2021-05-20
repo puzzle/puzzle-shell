81477882c919958f82835f0ae55b4d32147f316e
Mathis Hofer <hofer@puzzle.ch>
2021-05-20 09:52:07 +0200

    Add missing components to index.js

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ export { Banner } from "./src/components/Banner.js";
 export { Container } from "./src/components/Container.js";
 export { Footer } from "./src/components/Footer.js";
 export { Header } from "./src/components/Header.js";
+export { Hero } from "./src/components/Hero.js";
+export { Icon } from "./src/components/Icon.js";
 export { Topbar } from "./src/components/Topbar.js";
+export { TopbarLink } from "./src/components/TopbarLink.js";
