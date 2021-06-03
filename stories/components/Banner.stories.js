515ea3d4ad7fe957cd1727a8120aac7684ce9438
Mathis Hofer <hofer@puzzle.ch>
2021-06-03 16:53:07 +0200

    Use PascalCase for story names

--- stories/components/Banner.stories.js
+++ stories/components/Banner.stories.js
@@ -1,8 +1,6 @@
 import { html } from "lit-element";
 import "../../src/components/Banner.js";
 
-/* eslint-disable camelcase */
-
 export default {
   title: "Puzzle Shell/Components/Banner",
   parameters: {
@@ -12,9 +10,8 @@ export default {
 
 export const Default = () => html`<pzsh-banner></pzsh-banner>`;
 
-export const With_Search = () => html`<pzsh-banner></pzsh-banner>`;
+export const WithSearch = () => html`<pzsh-banner></pzsh-banner>`;
 
-export const With_Search_And_Add_Action = () =>
-  html`<pzsh-banner></pzsh-banner>`;
+export const WithSearchAndAddAction = () => html`<pzsh-banner></pzsh-banner>`;
 
-export const With_Navigation = () => html`<pzsh-banner></pzsh-banner>`;
+export const WithNavigation = () => html`<pzsh-banner></pzsh-banner>`;
