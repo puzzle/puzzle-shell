63da30a4b5a7645374ea8aee6302b6e1b67674fa
Claudia Asti <asti@puzzle.ch>
2024-01-08 16:28:01 +0100

    Add storybook a11y plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,12 @@
 import type { StorybookConfig } from "@storybook/web-components-vite";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/web-components-vite",
