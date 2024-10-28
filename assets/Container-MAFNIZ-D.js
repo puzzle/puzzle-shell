16de03f5b3dab3185074abd51c5a162c3c4f50d4
hupf <mathis@fsfe.org>
2024-10-28 12:59:28 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@014815b5882c9544e2ac1e4bc6c7d025af85e4fe 🚀

---
+++ assets/Container-MAFNIZ-D.js
@@ -0,0 +1,12 @@
+import{ae as n,af as s}from"./index-Dp7Qg1mk.js";import{u as r}from"./index-BjhThj5g.js";import"./iframe-BGN1PgLk.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";function o(t){const e={code:"code",h1:"h1",p:"p",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Puzzle Shell/Components/Container"}),`
+`,n.jsx(e.h1,{id:"container",children:"Container"}),`
+`,n.jsxs(e.p,{children:["The ",n.jsx(e.code,{children:"pzsh-container"})," component is the top-level component that implements the page layout. It contains all other Puzzle Shell components as well as the application's or site's content."]}),`
+`,n.jsx(e.p,{children:"Example usage:"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{children:`<pzsh-container>
+  <pzsh-topbar>...</pzsh-topbar>
+  <pzsh-banner>...</pzsh-banner>
+  <pzsh-hero>...</pzsh-hero>
+  <main>The content</main>
+  <pzsh-footer>...</pzsh-footer>
+<pzsh-container>
+`})})]})}function x(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{x as default};
---
+++ assets/index-Bvv9IoJQ.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-BjhThj5g.js";import"./index-Dp7Qg1mk.js";import"./iframe-BGN1PgLk.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
