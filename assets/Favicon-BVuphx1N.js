16b760c9f4d2f19ecf747a95b672a9f5c821c9ee
hupf <mathis@fsfe.org>
2024-11-11 10:17:59 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@03410e759d5af550e24cc2727065217e39a7ffa2 🚀

--- assets/Favicon-BVuphx1N.js
+++ assets/Favicon-BVuphx1N.js
@@ -1,4 +1,4 @@
-import{ae as n,af as s}from"./index-DW77z9uq.js";import{u as r}from"./index-DfXxWbks.js";import"./iframe-k4v_q_Oh.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";function a(i){const e={a:"a",code:"code",h1:"h1",p:"p",pre:"pre",...r(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Puzzle Shell/Components/Favicon"}),`
+import{ae as n,af as s}from"./index-CeYA7-MC.js";import{u as r}from"./index-Cq_P8hkQ.js";import"./iframe-Dlu0DvoT.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";function a(i){const e={a:"a",code:"code",h1:"h1",p:"p",pre:"pre",...r(),...i.components};return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Puzzle Shell/Components/Favicon"}),`
 `,n.jsx(e.h1,{id:"favicon",children:"Favicon"}),`
 `,n.jsxs(e.p,{children:["The Favicon and its variations for the ",n.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/Manifest",rel:"nofollow",children:"Web Application Manifest"})," can be found here:"]}),`
 `,`
--- assets/index-O5CssMPO.js
+++
@@ -1,1 +0,0 @@
-import{M as a,u as n}from"./index-DfXxWbks.js";import"./index-DW77z9uq.js";import"./iframe-k4v_q_Oh.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
