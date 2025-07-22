6bcd1d6633fcbca827c7e4344ab4b922104077be
hupf <mathis@fsfe.org>
2025-07-22 11:06:41 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@421b2e620073c96cfc2c47aab196feef10528b20 🚀

---
+++ assets/Nav.stories-Dn_zLOqt.js
@@ -0,0 +1,33 @@
+import{x as v}from"./lit-element-JDTHnSco.js";import"./Subnav-CZdZFk0G.js";import"./custom-element-BhZVzxrc.js";import"./theme-CU95s7Dv.js";import"./property-DflLml2K.js";import"./class-map-Cy6jXCQq.js";const g={title:"Puzzle Shell/Components/Nav",parameters:{layout:"fullscreen"}},e=()=>v`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+`,n=()=>v`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-subnav>
+      <pzsh-nav-item active>My Time Tracking</pzsh-nav-item>
+      <pzsh-nav-item>Reporting</pzsh-nav-item>
+    </pzsh-subnav>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+`;var s,a,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => html\`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+\``,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var i,p,m;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`() => html\`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-subnav>
+      <pzsh-nav-item active>My Time Tracking</pzsh-nav-item>
+      <pzsh-nav-item>Reporting</pzsh-nav-item>
+    </pzsh-subnav>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+\``,...(m=(p=n.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const u=["OneLevel","TwoLevels"];export{e as OneLevel,n as TwoLevels,u as __namedExportsOrder,g as default};
