4951676aa1ae9c21155fb46d8d3fbf42aa6da5af
hupf <mathis@fsfe.org>
2026-08-05 06:52:55 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@87c81bdd6c0d17052b2f34e13941eb2c9aaa280d 🚀

---
+++ assets/iframe-DoDVU0y1.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-CEXs4UB1.js";e();
--- assets/mdx-react-shim-R_2roI6Z.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-BdFrVu1K.js";import{k as t}from"./blocks-DMaJurS7.js";var n=e((()=>{t()}));export{n as t};
