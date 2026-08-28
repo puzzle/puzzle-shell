5c5a65467c113414065ad830d555b47daff29d6c
hupf <mathis@fsfe.org>
2026-08-28 14:20:08 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@45f723a70ad7ca2db3bab58a06b66366fe526ae8 🚀

---
+++ assets/Nav.stories-B0qljplp.js
@@ -0,0 +1,33 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{n as t,o as n}from"./iframe-B4opvsuG.js";import{n as r,r as i,t as a}from"./Subnav-DkHeVWjr.js";var o,s,c,l;function u(){return(u=e((()=>{t(),i(),r(),a(),o={title:`Puzzle Shell/Components/Nav`,parameters:{layout:`fullscreen`}},s=()=>n`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+`,c=()=>n`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-subnav>
+      <pzsh-nav-item active>My Time Tracking</pzsh-nav-item>
+      <pzsh-nav-item>Reporting</pzsh-nav-item>
+    </pzsh-subnav>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+`,s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`() => html\`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+\``,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`() => html\`
+  <pzsh-nav>
+    <pzsh-nav-item active>Time Tracking</pzsh-nav-item>
+    <pzsh-subnav>
+      <pzsh-nav-item active>My Time Tracking</pzsh-nav-item>
+      <pzsh-nav-item>Reporting</pzsh-nav-item>
+    </pzsh-subnav>
+    <pzsh-nav-item>Planning</pzsh-nav-item>
+    <pzsh-nav-item>Expenses</pzsh-nav-item>
+  </pzsh-nav>
+\``,...c.parameters?.docs?.source}}},l=[`OneLevel`,`TwoLevels`]})))()}u();export{s as OneLevel,c as TwoLevels,l as __namedExportsOrder,o as default};
