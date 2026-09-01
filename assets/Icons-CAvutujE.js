8a68d3f04beab00d5dd88f698eaba5607d98e3fa
hupf <mathis@fsfe.org>
2026-09-01 06:14:23 +0000

    Deploying to gh-pages from @ puzzle/puzzle-shell@d13d37c00bfddb3f7462fdc94a61b77c3e130e9b 🚀

---
+++ assets/Icons-CAvutujE.js
@@ -0,0 +1,19 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{M as t,a as n,j as r,o as i,y as a}from"./blocks-BbgAL4DG.js";import{n as o,t as s}from"./Icon-BSddS353.js";function c(e){let r={a:`a`,code:`code`,h1:`h1`,h2:`h2`,li:`li`,p:`p`,ul:`ul`,...t(),...e.components};return(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(n,{title:`Puzzle Shell/Components/Icons`}),`
+`,(0,u.jsx)(r.h1,{id:`icons`,children:`Icons`}),`
+`,(0,u.jsxs)(r.p,{children:[`The Puzzle Shell uses the `,(0,u.jsx)(r.a,{href:`https://iconscout.com/unicons`,rel:`nofollow`,children:`Unicons`}),` icon set, which is Apache 2.0 licensed.`]}),`
+`,(0,u.jsxs)(r.p,{children:[`It provides the following basic set of icons via the `,(0,u.jsx)(r.code,{children:`pzsh-icon`}),` component:`]}),`
+`,(0,u.jsx)(`div`,{children:Object.keys(s).map(e=>(0,u.jsxs)(`div`,{style:{display:`flex`,alignItems:`center`},children:[(0,u.jsx)(`pzsh-icon`,{name:e,style:{marginRight:`0.7rem`}}),(0,u.jsx)(`code`,{children:`<pzsh-icon name=\"${e}\"></pzsh-icon>`})]},e))}),`
+`,(0,u.jsxs)(r.p,{children:[`You can grab additional icons from the `,(0,u.jsx)(r.a,{href:`https://iconscout.com/unicons/explore/line`,rel:`nofollow`,children:`full list of icons`}),` and use them as SVGs in your project.`]}),`
+`,(0,u.jsx)(r.h2,{id:`optimization`,children:`Optimization`}),`
+`,(0,u.jsx)(r.p,{children:`When downloading icon SVGs, make sure to process them as follows:`}),`
+`,(0,u.jsxs)(r.ul,{children:[`
+`,(0,u.jsxs)(r.li,{children:[`Optimize them with `,(0,u.jsx)(r.a,{href:`https://jakearchibald.github.io/svgomg/`,rel:`nofollow`,children:`SVGOMG`}),`:`,`
+`,(0,u.jsxs)(r.ul,{children:[`
+`,(0,u.jsx)(r.li,{children:`Copy the SVGs markup`}),`
+`,(0,u.jsxs)(r.li,{children:[`Goto `,(0,u.jsx)(r.a,{href:`https://jakearchibald.github.io/svgomg/`,rel:`nofollow`,children:`SVGOMG`}),` and use "Paste markup"`]}),`
+`,(0,u.jsx)(r.li,{children:`Copy the reduced SVG (copy icon on the bottom right)`}),`
+`]}),`
+`]}),`
+`,(0,u.jsxs)(r.li,{children:[`Replace all `,(0,u.jsx)(r.code,{children:`fill="..."`}),` and `,(0,u.jsx)(r.code,{children:`stroke="..."`}),` color values with `,(0,u.jsx)(r.code,{children:`currentColor`}),` for the icon to use the actual CSS color`]}),`
+`,(0,u.jsx)(r.li,{children:`Use the icon 🚀`}),`
+`]})]})}function l(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,u.jsx)(n,{...e,children:(0,u.jsx)(c,{...e})}):c(e)}var u;function d(){return(d=e((()=>{u=a(),r(),i(),o()})))()}d();export{l as default};
---
+++ assets/iframe-JOqjNxiR.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-Dt3RJBzD.js";e();
